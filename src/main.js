26a014f247040b5352510c9a1bffd0580bf7b0ea
juanpabllo <juanpablo192.dreaw@gmail.com>
2020-11-10 14:29:02 -0300

    add: submit, get results and put results on html

---
+++ src/main.js
@@ -0,0 +1,40 @@
+const form = document.querySelector(".search-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let query = document.querySelector(".search-input").value;
+  query = query.trim();
+  getResults(query);
+}
+
+function getResults(query) {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=25&utf8=&format=json&srsearch=${query}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      putResults(data.query.search);
+    })
+    .catch((e) => console.log(`ÈRROR: ${e}`));
+}
+
+function putResults(sResults) {
+  const searchResults = document.querySelector(".results");
+  searchResults.innerHTML = "";
+  sResults.forEach((result) => {
+    //Generate a wikipedia page url for each result
+    const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
+
+    searchResults.insertAdjacentHTML(
+      "beforeend",
+      `<div class="result">
+      <h3 class="result-title">
+        <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+      </h3>
+      <span class="result-snippet">${result.snippet}</span><br>
+      <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+    </div>`
+    );
+  });
+}
